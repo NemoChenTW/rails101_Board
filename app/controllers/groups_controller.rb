1c4ef6819be6ae940615f3e82739999c735a971e
NemoChen <nemochentw@gmail.com>
2015-11-15 15:47:52 +0800

    前置設定: controller.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,4 +6,22 @@ class GroupsController < ApplicationController
 		flash[:warning] = "這是 warning 訊息！"
 	end
 
+	def show
+	end
+
+	def new
+	end
+
+	def edit
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
 end
