89c535e3cfbf7782653f234dffd745ad16418c10
NemoChen <nemochentw@gmail.com>
2015-11-30 16:56:23 +0800

    修改 post controller 'Create'.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,14 @@ class PostsController < ApplicationController
 	end
 
 	def create
+		@group = Group.find(params[:group_id])
+		@post = @group.posts.build(post_params)
+
+		if @post.save
+			redirect_to group_path(@group), notice: "新增文章成功！"
+		else
+			render :new
+		end
 	end
 
 	def update
