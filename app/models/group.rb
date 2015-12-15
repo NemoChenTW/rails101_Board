7c3240844801961728e7d42d58d237c6756bb103
NemoChen <nemochentw@gmail.com>
2015-12-15 21:35:38 +0800

    修正 拼字錯誤.

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,7 +3,7 @@ class Group < ActiveRecord::Base
 	# validates :title, presence: true	#此行也可, 訊息為預設英文
 	validates :title, presence: { message: '此欄位不可為空白!' }
 
-	has_many :posts, dependent: :destory
+	has_many :posts, dependent: :destroy
 	
 	belongs_to :owner, class_name: "User", foreign_key: :user_id
 
