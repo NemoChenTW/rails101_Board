6cc12c4d6c29ad412798dfd695f331f2cd0f4ca2
NemoChen <nemochentw@gmail.com>
2015-12-15 21:08:59 +0800

    修改 "db/migrate/20151215130731_add_posts_count_to_group.rb".

--- db/migrate/20151215130731_add_posts_count_to_group.rb
+++ db/migrate/20151215130731_add_posts_count_to_group.rb
@@ -1,5 +1,5 @@
 class AddPostsCountToGroup < ActiveRecord::Migration
   def change
-    add_column :groups, :posts_count, :integer
+    add_column :groups, :posts_count, :integer, default: 0
   end
 end
